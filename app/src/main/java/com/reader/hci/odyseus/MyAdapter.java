2aaea3363d680db578399934aced6352d405b972
davsobo <aureliousvayne57@gmail.com>
2016-12-15 16:41:09 +0700

    15/12/16-16:40

--- app/src/main/java/com/reader/hci/odyseus/MyAdapter.java
+++ app/src/main/java/com/reader/hci/odyseus/MyAdapter.java
@@ -31,15 +31,24 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
         //create a new view
         View v = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.my_text_view,parent,false);
+                .inflate(R.layout.info_text,parent,false);
         //set the view's size,margin,paddings and layout parameters
         //code here
         ViewHolder vh = new ViewHolder(v);
         return vh;
     }
 
+    //Replace the contecnt of a view invoked by the layout manager
     @Override
     public void onBindViewHolder(ViewHolder holder, int position){
+        //- get element from your dataset at this position
+        //- replace the content of the view with that element
+        holder.mTextView.setText(mDataset[position]);
+    }
 
+    //return the size of your dataset invoked by the layout manager
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
     }
 }
